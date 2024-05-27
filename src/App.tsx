3ab3be85dd621dc36598d6f2ec5f5b99348d4d0a
Luniminex <kubasmisek@seznam.cz>
2024-05-27 12:16:06 +0200

    Expand/Collapse of items done recursively

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import {tableOfContentsItems} from "./components/TableOfContents/data/tableOfContentsItems.ts";
+import {NOZ_TOC_ITEMS_MOCK} from "./components/TableOfContents/data/NOZ_TOC_ITEMS_MOCK.ts";
 import {useTableOfContents} from "./components/TableOfContents/useTableOfContents.ts";
 import {TableOfContents} from "./components/TableOfContents/TableOfContents.tsx";
 
 function App() {
-    const items = tableOfContentsItems;
+    const items = NOZ_TOC_ITEMS_MOCK;
     const props = useTableOfContents({ items });
 
     return (
--- src/components/TableOfContents/useTableOfContents.ts
+++ src/components/TableOfContents/useTableOfContents.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ContentItem } from './types/ContentItem.ts';
 
 type TableOfContentsArg = {
@@ -7,7 +8,38 @@ type TableOfContentsArg = {
 // TODO: expand/collapse of items
 // TODO: Collapse recursively
 export const useTableOfContents = ({ items }: TableOfContentsArg) => {
-    const onClick = (item: ContentItem) => () => console.log(item);
+    //use set because operations like delete and has should be faster than array
+    const [collapsed, setCollapsed] = useState<Set<string>>(new Set(items.map(item => item.id)));
 
-    return { items, onClick };
+    //helper function that recursively finds children of a given item
+    const getChildren = (id: string): string[] => {
+        let result: string[] = [];
+        //goes through all items and checks if the parentId is the given id
+        for (const item of items) {
+            if (item.parentId === id) {
+                result.push(item.id);
+                //since this is a child, we need to check if it has children as well
+                result = result.concat(getChildren(item.id));
+            }
+        }
+        return result;
+    };
+
+    const onClick = (item: ContentItem) => () => {
+        //new set to signal that the state has indeed changed
+        const newCollapsed = new Set(collapsed);
+        if (newCollapsed.has(item.id)) {
+            newCollapsed.delete(item.id);
+        } else {
+            //add the item and all its children to the collapsed set
+            newCollapsed.add(item.id);
+            getChildren(item.id).forEach(childId => newCollapsed.add(childId));
+        }
+        setCollapsed(newCollapsed);
+    }
+    
+    //return only the items that are not in the collapsed set
+    const visibleItems = items.filter(item => !collapsed.has(item.parentId ?? ''));
+
+    return { items: visibleItems, onClick };
 };
